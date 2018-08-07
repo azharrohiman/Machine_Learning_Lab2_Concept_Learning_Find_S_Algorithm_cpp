826ba8ccd0289b07e27da8509ad191abbab54ffa
Azhar Rohiman <rhmmuh005@myuct.ac.za>
2018-08-07 19:09:50 +0200

    Outputting to file

--- driver.cpp
+++ driver.cpp
@@ -9,18 +9,9 @@
 using namespace std;
 
 struct EnjoySport {
-	//string dataset[] = {""};
 	vector<string> dataset;
-	//int enjoy;
-
-	/*
-	EnjoySport() : sky ("0"), air_temp ("0"), humidity ("0"), wind ("0"), water ("0"), forecast ("0") {
-		
-	}
-	*/
 
 	EnjoySport(string sky, string air_temp, string humidity, string wind, string water, string forecast, string enjoy) {
-		//this->dataset = {sky, air_temp, humidity, wind, water, forecast, enjoy};
 		this->dataset.push_back(sky);
 		this->dataset.push_back(air_temp);
 		this->dataset.push_back(humidity);
@@ -36,19 +27,17 @@ void findS(vector<EnjoySport> training_examples) {
 	string hypothesis[] = {"0", "0", "0", "0", "0", "0", "0"};
 	int trace_count = 0;
 
-	cout << training_examples.size() << endl;
+	ofstream output_file;
+	output_file.open("output.txt");
 
 	for (int i = 0; i < training_examples.size(); i++) {
 		vector<string> example = training_examples.at(i).dataset;
 
+		output_file << "Trace: " << ++trace_count << endl;
+
 		if (example.at(example.size() - 1) == "1") {
 
-			cout << "Trace: " << ++trace_count << endl;
-
 			for (int j = 0; j < (sizeof(hypothesis)/sizeof(hypothesis[0])) - 1; j++) {
-
-				//cout << hypothesis[j] << endl;
-
 				if (hypothesis[j] == "0") {
 					if (i == 0) {
 						hypothesis[j] = example.at(j);
@@ -60,9 +49,23 @@ void findS(vector<EnjoySport> training_examples) {
 						hypothesis[j] = "?";
 					}
 				}
-
-				cout << j << " " << hypothesis[j] << endl;
 			}
+		}
+
+		output_file << "<";
+
+		for (int k = 0; k < (sizeof(hypothesis)/sizeof(hypothesis[0])) - 1; k++) {
+			if (k != (sizeof(hypothesis)/sizeof(hypothesis[0])) - 2) {
+				output_file << hypothesis[k] << ", ";
+			}
+
+			else {
+				output_file << hypothesis[k] << ">" << endl;
+			}
+		}
+
+		if (i != training_examples.size() - 1) {
+			output_file << endl;
 		}
 	}
 }
@@ -77,6 +80,7 @@ int main(int argc, char* argv[]) {
 	training_examples.push_back(EnjoySport("Sunny", "Warm", "High", "Strong", "Cool", "Change", "1"));
 
 	findS(training_examples);
+	cout << "Find-S algorithm running on training examples" << endl;
 
 	return 0;
 }
